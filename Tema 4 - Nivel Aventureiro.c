65f53da8f7f06c6bccdcf782b1fd9a326fa98450
Gisele Correia <giselehmcorreia@gmail.com>
2026-06-07 20:37:08 -0300

    atualizações

--- Tema 4 - Nivel Aventureiro.c
+++ Tema 4 - Nivel Aventureiro.c
@@ -9,12 +9,14 @@ int numero;
 int bispo;
 int torre;
 int rainha;
+int cavalo;
 
 //Entrada de dados
 printf("Digite o número da peça que deseja movimentar:\n");
 printf("[ 1 ] Bispo\n");
 printf("[ 2 ] Torre\n");
 printf("[ 3 ] Rainha\n");
+printf("[ 4 ] Cavalo\n");
 scanf(" %i", &numero);
 
 //Saída
@@ -37,6 +39,11 @@ else if (numero == 3){
     printf("Esquerda\n");
     }}
 
+cavalo = 1;
+ do {  
+    printf("Baixo e Esquerda\n");
+    cavalo++;
+    } while (cavalo<=5);
 
     return 0;
 }
